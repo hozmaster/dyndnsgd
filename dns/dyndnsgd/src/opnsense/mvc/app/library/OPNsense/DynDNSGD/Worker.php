acb730e5cda446f524c8d03821cd6303e61d5c1d
Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>
2021-06-26 22:46:06 +0300

    Add some commented php code

--- dns/dyndnsgd/src/opnsense/mvc/app/library/OPNsense/DynDNSGD/Worker.php
+++ dns/dyndnsgd/src/opnsense/mvc/app/library/OPNsense/DynDNSGD/Worker.php
@@ -32,6 +32,8 @@ use Common;
 
 class Worker extends Common
 {
+    private $staging_url = 'api.ote-godaddy.com';
+    private $production_url = "api.godaddy.com";
 
     public function __construct(string $uuid)
     {
@@ -43,4 +45,27 @@ class Worker extends Common
     {
     }
 
+    // send request to
+    private function sendRequest()
+    {
+
+        //        $url = 'http://server.com/path';
+        //        $data = array('key1' => 'value1', 'key2' => 'value2');
+        //
+        //        // use key 'http' even if you send the request to https://...
+        //        $options = array(
+        //            'http' => array(
+        //                'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        //                'method' => 'POST',
+        //                'content' => http_build_query($data)
+        //            )
+        //        );
+        //
+        //        $context = stream_context_create($options);
+        //        $result = file_get_contents($url, false, $context);
+        //        if ($result === FALSE) {
+        //            echo "failed";
+        //        }
+
+    }
 }
