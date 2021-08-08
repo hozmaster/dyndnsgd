5b273be9b018c8efc0f2b21d970ecb0ce44d85d2
Olli-Pekka Wallin <hozmaster@users.noreply.github.com>
2021-08-08 22:32:47 +0300

    Small changes tp project support files (#7)
    * Make small changes to pkg-descr
    
    * Bump up  pkg version 0.3.0-beta. Move setup.sh away from actal projetc folder structure
    
    Co-authored-by: Olli-Pekka Wallin <olli-pekka.wallin@wallin.biz>

--- dns/setup.sh
+++ dns/setup.sh
@@ -1,9 +1,35 @@
 #!/bin/sh
 
+#
+# Copyright (c) 2021, Olli-Pekka Wallin
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+#    list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+#    this list of conditions and the following disclaimer in the documentation
+#    and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+
 # This file is for helping development work.
 
 TARGET_BASE_PATH=/usr/local/opnsense/mvc/app
-SOURCE_BASE_PATH=src/opnsense/mvc/app
+SOURCE_BASE_PATH=dyndnsgd/src/opnsense/mvc/app
 
 rm -r $TARGET_BASE_PATH/models/OPNsense/DynDNSGD
 rm -r $TARGET_BASE_PATH/views/OPNsense/DynDNSGD
@@ -59,11 +85,11 @@ cp -vf $SOURCE_BASE_PATH/controllers/OPNsense/DynDNSGD/Api/* $TARGET_BASE_PATH/c
 cp -vf $SOURCE_BASE_PATH/library/OPNsense/DynDNSGD/*.php $TARGET_BASE_PATH/library/OPNsense/DynDNSGD/
 
 ## script
-cp -vf src/opnsense/scripts/* /usr/local/opnsense/scripts/OPNsense/DynDNSGD
+cp -vf dyndnsgd/src/opnsense/scripts/* /usr/local/opnsense/scripts/OPNsense/DynDNSGD
 chmod a+x /usr/local/opnsense/scripts/OPNsense/DynDNSGD/*.php
 
 ## service
-cp -vf src/opnsense/service/conf/actions.d/actions_dyndnsgd.conf /usr/local/opnsense/service/conf/actions.d/actions_dyndnsgd.conf
+cp -vf dyndnsgd/src/opnsense/service/conf/actions.d/actions_dyndnsgd.conf /usr/local/opnsense/service/conf/actions.d/actions_dyndnsgd.conf
 
 ## legacy plugins support
 cp -vf usr/local/etc/rc.dyndnsgd /usr/local/etc/rc.dyndnsgd
@@ -75,4 +101,4 @@ cp -vf src/etc/inc/plugins.inc.d/dyndnsgd/RequesterBase.inc /usr/local/etc/inc/p
 if [ ! -d /usr/local/etc/inc/plugins.inc.d/dyndnsgd ]; then
   mkdir /usr/local/etc/inc/plugins.inc.d/dyndnsgd
 fi
-cp -vf src/etc/inc/plugins.inc.d/dyndnsgd/gdDnsUpdater.inc /usr/local/etc/inc/plugins.inc.d/dyndnsgd/gdDnsUpdater.inc
+cp -vf dyndnsgd/src/etc/inc/plugins.inc.d/dyndnsgd/gdDnsUpdater.inc /usr/local/etc/inc/plugins.inc.d/dyndnsgd/gdDnsUpdater.inc
